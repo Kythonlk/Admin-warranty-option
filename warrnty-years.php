2a6719ef9e1336014fef38ac337004f2cce457e2
Kythonlk <109597262+Kythonlk@users.noreply.github.com>
2023-06-07 12:12:44 +0400

    Update warrnty-years.php

--- warrnty-years.php
+++ warrnty-years.php
@@ -1,28 +1,27 @@
 <?php
 /**
- * Plugin Name: warranty Option woocommerce
+ * Plugin Name: Warranty Option for WooCommerce
  * Description: Add custom warranty options to WooCommerce products.
  * Version: 1.0.1
  * Author: Kythonlk(Kavindu Harshana)
  * Author URI: https://kythonlk.com
- * Text Domain: kypow
- * Domain Path: /languages
+ * Text Domain: kythonlk-plugins
  */
 
 // Activation and deactivation hooks
-register_activation_hook( __FILE__, 'kypow_activation_function' );
-register_deactivation_hook( __FILE__, 'kypow_deactivation_function' );
+register_activation_hook( __FILE__, 'warranty_option_activation_function' );
+register_deactivation_hook( __FILE__, 'warranty_option_deactivation_function' );
 
-function kypow_activation_function() {
+function warranty_option_activation_function() {
     // Perform activation tasks, if any
-    add_option( 'kypow_enable_warranty', true );
-    add_option( 'kypow_enable_custom_text', true );
+    add_option( 'warranty_option_enable_warranty', true );
+    add_option( 'warranty_option_enable_custom_text', true );
 }
 
-function kypow_deactivation_function() {
+function warranty_option_deactivation_function() {
     // Perform deactivation tasks, if any
-    delete_option( 'kypow_enable_warranty' );
-    delete_option( 'kypow_enable_custom_text' );
+    delete_option( 'warranty_option_enable_warranty' );
+    delete_option( 'warranty_option_enable_custom_text' );
 }
 
 // Load admin settings file
